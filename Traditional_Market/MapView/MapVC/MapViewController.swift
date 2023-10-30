453e40023925b310aac3d742e0cbcdc53ff6dcfb
yeomsungpil <present7256@gmail.com>
2023-10-30 16:57:42 +0900

    MapViewController 네비게이션 영역에 시장 컬렉션 버튼 생성

--- Traditional_Market/MapView/MapVC/MapViewController.swift
+++ Traditional_Market/MapView/MapVC/MapViewController.swift
@@ -356,6 +356,12 @@ extension MapViewController {
         searchController.delegate = self
         searchController.searchResultsUpdater = self
         searchController.searchBar.delegate = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .plain, target: self, action: #selector(favoriteBarButtonClicked))
+    }
+    
+    @objc func favoriteBarButtonClicked() {
+        let saveMarket = SaveMarketViewController()
+        navigationController?.pushViewController(saveMarket, animated: true)
     }
     
     
