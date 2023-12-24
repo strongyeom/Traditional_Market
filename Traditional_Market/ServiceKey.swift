f041e2163f9809c4093850cece0272e7ae1b92ab
yeomsungpil <present7256@gmail.com>
2023-12-24 15:54:49 +0900

    API key 정리중

---
+++ Traditional_Market/ServiceKey.swift
@@ -0,0 +1,16 @@
+//
+//  ServiceKey.swift
+//  Traditional_Market
+//
+//  Created by 염성필 on 2023/12/24.
+//
+
+import Foundation
+
+enum ServiceKey {
+    // 공공데이터 포털 디코딩 키
+    static let marketKey = "UQybVf/BVAsYaLeP9xLFftf6XvnhuY3nWPU72kKjfSPporofcAGn6UQp+G9ftv2cI9VQxA9Fq0B/tIWxZ2lavg=="
+    static let naverKey = "myXCWsXxrg83Q4L0SAdP"
+    static let naverSecretKey = "2s8Jgd07Ij"
+    static let koreaTourKey = "UQybVf/BVAsYaLeP9xLFftf6XvnhuY3nWPU72kKjfSPporofcAGn6UQp+G9ftv2cI9VQxA9Fq0B/tIWxZ2lavg=="
+}
