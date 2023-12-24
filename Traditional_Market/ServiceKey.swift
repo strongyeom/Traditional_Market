96faabde5705afe03ce3979b1334796c93aa128c
yeomsungpil <present7256@gmail.com>
2023-12-24 20:56:09 +0900

    git 해결완료

--- Traditional_Market/ServiceKey.swift
+++ Traditional_Market/ServiceKey.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum ServiceKey {
-    // 공공데이터 포털 디코딩 키
+    // 공공데이터 포털 키
     static let marketKey = "UQybVf/BVAsYaLeP9xLFftf6XvnhuY3nWPU72kKjfSPporofcAGn6UQp+G9ftv2cI9VQxA9Fq0B/tIWxZ2lavg=="
     static let naverKey = "myXCWsXxrg83Q4L0SAdP"
     static let naverSecretKey = "2s8Jgd07Ij"
